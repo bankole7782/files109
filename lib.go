20413fe8bb78f79d31d7829aea15e5d629f621d8
Bankole Ojo <ojobankole@gmail.com>
2024-03-16 15:27:58 +0100

    Write and Read file

--- lib.go
+++ lib.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -75,22 +76,28 @@ func WriteFile(linuxFolderPath, partitionName, name string, data []byte) error {
 		return err
 	}
 
-	usedSizeOfIndexPt := getWrittenIndexPartition(indexPtElems)
-
 	_, dataPartitionPath := getPartitionFiles(linuxFolderPath, partitionName)
+
+	usedSizeOfIndexPt := getWrittenIndexPartition(indexPtElems)
+	dataEnd := usedSizeOfIndexPt + int64(len(data)) + 1
+	dataPtSize := getFileSize(dataPartitionPath)
+	if dataEnd > dataPtSize {
+		return errors.New("you have reached the end of the partition")
+	}
+
 	dataPartitionHandle, err := os.OpenFile(dataPartitionPath, os.O_WRONLY, 0666)
 	if err != nil {
 		return errors.Wrap(err, "os error")
 	}
 	defer dataPartitionHandle.Close()
 
-	_, err = dataPartitionHandle.Write(data)
+	_, err = dataPartitionHandle.WriteAt(data, usedSizeOfIndexPt+1)
 	if err != nil {
 		return errors.Wrap(err, "os error")
 	}
 
-	indexPtElem := IndexPartitionElem{FileName: name, DataBegin: usedSizeOfIndexPt,
-		DataEnd: usedSizeOfIndexPt + int64(len(data))}
+	indexPtElem := IndexPartitionElem{FileName: name, DataBegin: usedSizeOfIndexPt + 1,
+		DataEnd: dataEnd}
 
 	indexPtElems = append(indexPtElems, indexPtElem)
 
@@ -163,10 +170,60 @@ func ReadIndexPartition(linuxFolderPath, partitionName string) ([]IndexPartition
 		}
 	}
 
-	fmt.Println(headerBytes)
-
 	// parse the header
-	return emptyElems, nil
+	ret, _ := parseIndexPartitionString(string(headerBytes))
+	return ret, nil
+}
+
+func parseIndexPartitionString(load string) ([]IndexPartitionElem, error) {
+	ret := make([]IndexPartitionElem, 0)
+
+	cleanedF1File := strings.ReplaceAll(string(load), "\r", "")
+	partsOfRawF1File := strings.Split(cleanedF1File, "\n\n")
+	for _, part := range partsOfRawF1File {
+		innerParts := strings.Split(strings.TrimSpace(part), "\n")
+
+		var elem IndexPartitionElem
+		for _, line := range innerParts {
+			var colonIndex int
+			for i, ch := range line {
+				if fmt.Sprintf("%c", ch) == ":" {
+					colonIndex = i
+					break
+				}
+			}
+
+			if colonIndex == 0 {
+				continue
+			}
+
+			optName := strings.TrimSpace(line[0:colonIndex])
+			optValue := strings.TrimSpace(line[colonIndex+1:])
+
+			if optName == "data_key" {
+				elem.FileName = optValue
+			} else if optName == "data_begin" {
+				data, err := strconv.ParseInt(optValue, 10, 64)
+				if err != nil {
+					return ret, errors.New("data_begin is not of type int64")
+				}
+				elem.DataBegin = data
+			} else if optName == "data_end" {
+				data, err := strconv.ParseInt(optValue, 10, 64)
+				if err != nil {
+					return ret, errors.New("data_end is not of type int64")
+				}
+				elem.DataEnd = data
+			}
+		}
+
+		if elem.FileName == "" {
+			continue
+		}
+		ret = append(ret, elem)
+	}
+
+	return ret, nil
 }
 
 func getFileSize(path string) int64 {
@@ -180,4 +237,32 @@ func getWrittenIndexPartition(indexPartitionElems []IndexPartitionElem) int64 {
 	}
 
 	return indexPartitionElems[len(indexPartitionElems)-1].DataEnd
+}
+
+func ReadFile(linuxFolderPath, partitionName, name string) ([]byte, error) {
+	elems, err := ReadIndexPartition(linuxFolderPath, partitionName)
+	if err != nil {
+		return nil, err
+	}
+
+	var fileElem IndexPartitionElem
+	for _, elem := range elems {
+		if elem.FileName == name {
+			fileElem = elem
+			break
+		}
+	}
+
+	fileData := make([]byte, fileElem.DataEnd-fileElem.DataBegin)
+
+	_, dataPartitionPath := getPartitionFiles(linuxFolderPath, partitionName)
+	dataPartitionHandle, err := os.OpenFile(dataPartitionPath, os.O_RDONLY, 0666)
+	if err != nil {
+		return nil, errors.Wrap(err, "os error")
+	}
+	defer dataPartitionHandle.Close()
+
+	dataPartitionHandle.ReadAt(fileData, fileElem.DataBegin)
+
+	return fileData, nil
 }
--- t1/main.go
+++ t1/main.go
@@ -1,16 +1,43 @@
 package main
 
-import "github.com/bankole7782/files109"
+import (
+	"fmt"
+
+	"github.com/bankole7782/files109"
+)
 
 func main() {
 	rootFolder, ptName := "/tmp", "test1"
-	// err := files109.CreatePartition(rootFolder, ptName, 1, 10)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	err := files109.WriteFile(rootFolder, ptName, "james.txt", []byte("James Ojo.\n"))
+	err := files109.CreatePartition(rootFolder, ptName, 1, 10)
 	if err != nil {
 		panic(err)
 	}
+
+	err = files109.WriteFile(rootFolder, ptName, "jas.txt", []byte("Ja Ojo.\n"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = files109.WriteFile(rootFolder, ptName, "hope.txt", []byte("Hoper Hope.\n"))
+	if err != nil {
+		panic(err)
+	}
+
+	files := []string{"james.txt", "jas.txt", "hope.txt"}
+	for _, f := range files {
+		data, err := files109.ReadFile(rootFolder, ptName, f)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		fmt.Println(string(data))
+	}
+
+	elems, err := files109.ReadIndexPartition(rootFolder, ptName)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(elems)
 }
